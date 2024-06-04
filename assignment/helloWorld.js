c77c3931f86ee134000852edccb70e6430292c6f
Ben Pollack <benpollack8723@gmail.com>
2024-06-04 10:34:37 -0500

    updated html file and added to css

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -1,7 +1,1 @@
-const title = document.getElementById('title');
 
-window.onload = titleChange;
-
-const titleChange = () => {
-document.title.innerHTML = document.title.replace('Unit 1 Assignment', 'Hello World!')
-}
